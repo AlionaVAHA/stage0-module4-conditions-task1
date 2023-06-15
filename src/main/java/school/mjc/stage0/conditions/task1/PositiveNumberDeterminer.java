430d14322777363fef7c616291dbcba09073627d
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 19:46:00 -0400

    Update PositiveNumberDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+          boolean trueOrFalse;
+        if(numberToBeDetermined>0){
+            trueOrFalse= true;
+            System.out.println(trueOrFalse);
+        }
+        else {
+            trueOrFalse=false;
+            System.out.println(trueOrFalse);
+        }
     }
 }
